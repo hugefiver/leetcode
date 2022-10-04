a4ba6a336a48f67eb4b9a1310f6dc8c012563a12
Hugefiver <i@iruri.moe>
2022-10-04 18:04:16 +0800

    p112 path-sum

---
+++ src/bin/p112-path-sum.rs
@@ -0,0 +1,58 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn has_path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> bool {
+        if let Some(root) = root {
+            let val = root.borrow().val;
+            let mut node = root.borrow_mut();
+            if node.left.is_none() && node.right.is_none() {
+                val == target_sum
+            } else {
+                Self::has_path_sum(node.left.take(), target_sum - val)
+                    || Self::has_path_sum(node.right.take(), target_sum - val)
+            }
+        } else {
+            false
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+struct Solution;
+
+fn main() {}
