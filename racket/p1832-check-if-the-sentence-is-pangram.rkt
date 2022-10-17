167168a2c7b6f4bdc099e15706ffb138837cac99
Hugefiver <i@iruri.moe>
2022-10-17 17:48:21 +0800

    p1832 check-if-the-sentence-is-pangram rkt

---
+++ racket/p1832-check-if-the-sentence-is-pangram.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(require racket/set)
+
+(define/contract (check-if-pangram sentence)
+  (-> string? boolean?)
+    (let [(set (mutable-set))]
+        (for [(ch (in-string sentence))]
+            (set-add! set ch))
+        (eq? (set-count set) 26)))
