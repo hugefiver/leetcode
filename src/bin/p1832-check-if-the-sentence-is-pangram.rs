c1dcbde2a372e6d12dca5197e9b29dc1e6414170
Hugefiver <i@iruri.moe>
2022-10-17 08:32:46 +0800

    p1832 check-if-the-sentence-is-pangram

--- src/bin/p1832-check-if-the-sentence-is-pangram.rs
+++ src/bin/p1832-check-if-the-sentence-is-pangram.rs
@@ -1,5 +1,17 @@
 impl Solution {
     pub fn check_if_pangram(sentence: String) -> bool {
+        let mut arr = [false; 26];
+
+        for c in sentence.bytes() {
+            arr[(c - b'a') as usize] = true;
+        }
+
+        arr.into_iter().fold(true, |acc, x| acc && x)
+    }
+}
+
+impl Solution2 {
+    pub fn check_if_pangram(sentence: String) -> bool {
         let mut arr = [0; (b'z' - b'a' + 1) as usize];
 
         for c in sentence.bytes() {
@@ -11,5 +23,6 @@ impl Solution {
 }
 
 struct Solution;
+struct Solution2;
 
 fn main() {}
