62544ab92b3e0d13c70cc5ff7e1fb45506c7ca43
Hugefiver <i@iruri.moe>
2022-10-28 19:46:56 +0800

    p49 group-anagrams

---
+++ src/bin/p49-group-anagrams.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let xs = strs.into_iter().map(|s| {
+            let mut m = [0; 26];
+            s.as_bytes().iter().for_each(|x| {
+                m[(x - b'a') as usize] += 1;
+            });
+            (m, s)
+        }).fold(HashMap::new(), |mut acc, (m, s)| {
+            let v = acc.entry(m).or_insert(Vec::new());
+            v.push(s);
+            acc
+        });
+        xs.into_iter().map(|(_, v)| v).collect()
+    }
+}
+
+struct Solution;
+
+fn main() {}
