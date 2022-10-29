c0f460afbd341fc4a7aebb43760e41a88446e46f
Hugefiver <i@iruri.moe>
2022-10-29 11:11:40 +0800

    p2136 earliest-possible-day-of-full-bloom

--- src/bin/p2136-earliest-possible-day-of-full-bloom.rs
+++ src/bin/p2136-earliest-possible-day-of-full-bloom.rs
@@ -1,5 +1,20 @@
 impl Solution {
     pub fn earliest_full_bloom(plant_time: Vec<i32>, grow_time: Vec<i32>) -> i32 {
+        let mut idx = grow_time
+            .iter()
+            .zip(plant_time.iter())
+            .map(|(&x, &y)| (-x, -y))
+            .collect::<Vec<_>>();
+        idx.sort_unstable();
+        let (mut t, mut ans) = (0, 0);
+        for (x, y) in idx.into_iter() {
+            t -= y;
+            ans = ans.max(t - x);
+        }
+        ans
+    }
+
+    pub fn earliest_full_bloom_2(plant_time: Vec<i32>, grow_time: Vec<i32>) -> i32 {
         let n = plant_time.len();
         let mut plant_data: Vec<(i32, i32)> =
             plant_time.into_iter().zip(grow_time.into_iter()).collect();
